f06c13ab79937a804bdbeeb50eeff604b271bfc0
Asitha Navaratne <asithan476@gmail.com>
2025-11-20 08:53:24 +0530

    Finished styles for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import Button from "@/components/Button";
+import styles from "./page.module.scss";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className={styles["home-page"]}>
+      <h1 className={styles["home-page__title"]}>Collaborate and Create!</h1>
+      <h2 className={styles["home-page__sub-title"]}>
+        Work on your ideas together, seamlessly and in real time.
+      </h2>
+      <Button>Sign in to continue</Button>
+    </div>
+  );
 }
