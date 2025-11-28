5521b97bfd8b9c5649b79c570e69ad1967a227d8
Asitha Navaratne <asithan476@gmail.com>
2025-11-29 01:59:26 +0530

    Added button animations

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,13 +1,15 @@
+import styles from "./Button.module.css";
+
 import { ButtonProps } from "./Button.types";
 
 const Button = (props: ButtonProps) => {
   return (
     <button
       {...props}
-      className={`font-medium min-w-30 px-lg py-xs rounded-3xl border-primary border-3 cursor-pointer ${
+      className={`font-medium min-w-30 px-lg py-xs rounded-3xl border-primary border-3 duration-300 cursor-pointer ${
         props.variant === "secondary"
-          ? "bg-white text-primary"
-          : "bg-primary text-white"
+          ? styles["button-secondary"]
+          : styles["button-primary"]
       }`}
     >
       {props.children}
