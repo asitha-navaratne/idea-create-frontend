4755b1b2a65ca25604167ed89b397961321e2b06
Asitha Navaratne <asithan476@gmail.com>
2025-12-10 22:21:55 +0530

    Added toggle for dark mode

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+
 import logo from "@/public/images/logo.png";
 
 import Button from "../Button";
@@ -10,7 +13,11 @@ const Header = () => {
       <div className="relative h-6 w-42">
         <Image src={logo} sizes="300px" fill alt="IdeaCreate Logo" />
       </div>
-      <div className="flex gap-4 [&>*:first-child]:hidden sm:[&>*:first-child]:block">
+      <div className="flex gap-4 [&>*:nth-child(2)]:hidden sm:[&>*:nth-child(2)]:block">
+        <div className="absolute bottom-5 left-5 md:static flex items-center space-x-4 mr-4">
+          <Switch id="dark-mode" />
+          <Label htmlFor="dark-mode">Dark Mode</Label>
+        </div>
         <Button variant="secondary">Sign up</Button>
         <Button>Log in</Button>
       </div>
